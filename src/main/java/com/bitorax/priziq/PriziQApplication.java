4b524f9c0ae81570ab65efe843a42902b791500f
Quách Phú Thuận <thuanmobile1111@gmail.com>
2025-03-15 16:23:54 +0700

    init: PriziQ! Project

---
+++ src/main/java/com/bitorax/priziq/PriziQApplication.java
@@ -0,0 +1,13 @@
+package com.bitorax.priziq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriziQApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriziQApplication.class, args);
+    }
+
+}
